d3ae0192d1922056cfd4ea9c861d5346e408069e
Mustaque Nadim <mustaquenadim2000@gmail.com>
2021-02-08 12:43:29 +0600

    bug fix

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const searchBtn = document.getElementById('search-btn');
 const alert = document.getElementById('alert');
-const mealItems = document.getElementById('meal-items');
 
 // search
 searchBtn.addEventListener('click', () => {
     const inputMeal = document.getElementById('meal-name').value;
-    mealItems.innerHTML = '';
     if (inputMeal != '')
     {
-        // console.log('search if')
         alert.style.display = 'block';
         getMeals(inputMeal);
     } 
     else
     {
-        // console.log('search else');
         alert.style.display = 'none';
     }
 });
@@ -24,11 +20,9 @@ var apiBase = 'https://www.themealdb.com/api/json/v1/1/search.php';
 
 // calling API
 const getMeals = (meals) => {
-    fetch(`${apiBase}?f=${meals}`)
     fetch(`${apiBase}?s=${meals}`)
         .then(response => response.json())
         .then(data => {
-            // console.log('api calling');
             mealList(data.meals);
         });
 };
@@ -37,24 +31,23 @@ const getMeals = (meals) => {
 const mealList = meal => {
     if (meal != null)
     {
-        // console.log('food item if');
         alert.style.display = 'none'
         meal.forEach(item => {
             let mealItem = `
             <div onclick="ingredients('${item.idMeal}')">
             <img src="${item.strMealThumb}">
-            <h4>${item.strMeal}</h4>
+            <h3>${item.strMeal}</h3>
             </div>
             `;
             let createDiv = document.createElement('div');
-            createDiv.className = 'image-with-title';
+            createDiv.className = 'meal-item';
             createDiv.innerHTML = mealItem;
-            document.getElementById('meal-items').appendChild(createDiv);
+            document.getElementById('food-items').appendChild(createDiv);
+            // document.getElementById('food-items').appendChild(mealItem);
         });
     }
     else
     {
-        // console.log('food item else');
         alert.style.display = 'block';
     }
 };
@@ -66,39 +59,59 @@ const ingredients = (id) => {
 		.then(response => response.json())
 		.then(data => {
             console.log(data.meals[0]);
-			ingredientsDisplay(data.meals[0]);
+			recipeDisplay(data.meals[0]);
 		});
 };
 
-// 
-let ingredientsDisplay = (data) => {
+// showing recipe
+let recipeDisplay = (recipe) => {
     console.log('babu tomar ki hoise');
-	document.getElementById('meal-list').style.display = 'none';
-	let fullDescription = `
-    <div class="ingredients">
+	document.getElementById('food-items').style.display = 'none';
+    document.getElementById('alert').style.display = 'none';
+	let ingredients = `
         <div>
-            <img src="${data.strMealThumb}">
+            <img src="${recipe.strMealThumb}">
         </div>
-        <div>
-            <h2>${data.strMeal}</h2>
-            <p>Ingredients</p>
-            <h5>+ ${data.strMeasure1}, ${data.strIngredient1}</h5>
-            <h5>+ ${data.strMeasure2}, ${data.strIngredient2}</h5>
-            <h5>+ ${data.strMeasure3}, ${data.strIngredient3}</h5>
-            <h5>+ ${data.strMeasure4}, ${data.strIngredient4}</h5>
-            <h5>+ ${data.strMeasure5}, ${data.strIngredient5}</h5>
-            <h5>+ ${data.strMeasure6}, ${data.strIngredient6}</h5>
-            <h5>+ ${data.strMeasure7}, ${data.strIngredient7}</h5>
-            <h5>+ ${data.strMeasure8}, ${data.strIngredient8}</h5>
+        <div class="content">
+            <h2>${recipe.strMeal}</h2>
+            <h6>Ingredients</h6>
+            <h5>+ ${recipe.strMeasure1}, ${recipe.strIngredient1}</h5>
+            <h5>+ ${recipe.strMeasure2}, ${recipe.strIngredient2}</h5>
+            <h5>+ ${recipe.strMeasure3}, ${recipe.strIngredient3}</h5>
+            <h5>+ ${recipe.strMeasure4}, ${recipe.strIngredient4}</h5>
+            <h5>+ ${recipe.strMeasure5}, ${recipe.strIngredient5}</h5>
         </div>
-    </div>
     `;
 
-    // create Div:
-	let createDiv = document.createElement("div");
-	createDiv.className = "Description";
-	createDiv.id = "Description";
-	createDiv.innerHTML = fullDescription;
-
-	document.getElementById('meal-items').appendChild(createDiv);
+	let recipeDiv = document.createElement("div");
+	recipeDiv.className = "ingredients";
+	recipeDiv.innerHTML = ingredients;
+	document.getElementById('recipe').appendChild(recipeDiv);
 };
+
+// const renderFoodInfo = (recipe) => {
+//     // Get all ingredients from the object. Up to 20
+//     const ingredients = [];
+//     for (let i = 1; i <= 20; i++) 
+//     {
+//         if (recipe[`strIngredient${i}`]) 
+//         {
+//             ingredients.push(`${recipe.strIngredient`${i}`}` - `${recipe.strMeasure`${i}`}`);
+//         }
+//         else 
+//         {
+//             // Stop if there are no more ingredients
+//             break;
+//         }
+//     }
+//     const recipeDiv = document.getElementById('foodsDetails');
+//     recipeDiv.innerHTML = `
+//     <img class="img-fluid rounded mb-4" src="${food.strMealThumb}" alt="">
+//     <h4>${food.strMeal}</h4>
+    
+//     <h5 class="pt-3 pb-2">Ingredients</h5>
+//     <ul class="list-unstyled mb-0">
+//     ${ingredients.map((ingredient) => <li>${ingredient}</li>).join('')}
+//     </ul>
+// `;
+// };
